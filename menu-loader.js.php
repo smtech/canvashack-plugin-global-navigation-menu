fa8efa3e1103fe009b6b3cbfcf36fa543c597268
Seth Battis <sethbattis@stmarksschool.org>
2016-05-10 15:09:18 -0400

    More careful CLI paths

--- menu-loader.js.php
+++ menu-loader.js.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once 'common.inc.php';
+require_once __DIR__ . '/common.inc.php';
 
 ?>
 var canvashack = {
 	loadMenus: function() {
-		$('body').append('<script src="<?= $pluginMetadata['PLUGIN_URL'] ?>/global-navigation-menu/navigation-menu.js.php?user_id=' + $('#identity .user_id').html()) + '"></script>');
+		$('body').append('<script src="<?= $pluginMetadata['PLUGIN_URL'] ?>/navigation-menu.js.php?user_id=' + $('#identity .user_id').html() + '"></script>');
 	}
 };
