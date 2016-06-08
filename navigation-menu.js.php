5c08db9b8aa53d386328fc082e2ec60dad0602c7
Seth Battis <sethbattis@stmarksschool.org>
2016-06-08 15:40:17 -0400

    New UI

--- navigation-menu.js.php
+++ navigation-menu.js.php
@@ -1,232 +1,199 @@
 <?php
 
+
 header('Content-Type: application/javascript');
 
-require_once('common.inc.php');
+require_once __DIR__ . '/common.inc.php';
 
 $cache->pushKey(basename(__FILE__, '.php'));
-
-if (isset($_REQUEST['user_id'])) {
-	if (is_numeric($_REQUEST['user_id'])) {
-		$userId = $_REQUEST['user_id'];
-	} else {
-		$userId = preg_replace('|.*users/(\d+)/?.*|', '$1', $_REQUEST['user_id']);
-	}
-}
-
-if (empty($userId)) {
-	exit;
-}
-
-/* get the current user's preferences */
-$response = $customPrefs->query("
-	SELECT * FROM `users`
-		WHERE
-			`id` = '" . $userId . "'
-");
-$userPrefs = $response->fetch_assoc();
-// FIXME groups are now stored in separate tables
-//$userPrefs['groups'] = unserialize($userPrefs['groups']);
-
-/* build our list of courses that should be removed from the Courses menu */
-$response = $customPrefs->query("
-	SELECT * FROM `courses`
-		WHERE
-			`menu-visible` = '0'
-");
-while ($c = $response->fetch_assoc()) {
-	$coursesToHide[] = $c['id'];
-}
-
-function isMenu($menuItem) {
-	return !is_numeric($menuItem['menu']);
-}
-
-function isColumn($menuItem) {
-	return !is_numeric($menuItem['column']) && !isMenu($menuItem);
-}
-
-function isSection($menuItem) {
-	return !is_numeric($menuItem['section']) && !isColumn($menuItem);
-}
-
-function isItem($menuItem) {
-	return is_numeric($menuItem['section']);
-}
-
-function nonempty($flag, $value) {
-	return (strlen($flag) ? $value : '');
-}
-
-function startMenu($html, $menuItem, $columns = 1) {
-	global $userPrefs, $pluginMetadata;
-	
-	return $html . '<li class="menubar-item"><a' . nonempty($menuItem['target'], " target=\"{$menuItem['target']}\"") . ' href="' . (empty($menuItem['url']) ? '#' : $pluginMetadata['PLUGIN_URL'] . "/click.php?item={$menuItem['id']}&user_id={$userPrefs['id']}&location=@@LOCATION@@") . '">' . $menuItem['title'] . '</a>';
-}
-
-function endMenu($html) {
-	return $html . (preg_match('%</li>$%', $html) ? '</ul>' : '') . '</li>';
-}
-
-function startColumn($html, $menuItem) {
-	return $html . (preg_match('%<!-- column ends -->$%', $html) ? '' : '<ul>') . "<!-- column {$menuItem['column']} begins -->";
-}
-
-function endColumn($html) {
-	return $html . '<!-- column ends -->';
-}
-
-function startSection($html, $menuItem) {
-	return $html . nonempty($menuItem['title'], "<li><a href=\"#\" class=\"section-title noclick\"><strong>{$menuItem['title']}</strong></a></li>");
-}
-
-function endSection($html) {
-	return $html;
-}
-
-function menuItem($html, $menuItem) {
-	global $userPrefs, $pluginMetadata;
-	return $html .  "<li><a" . nonempty($menuItem['target'], " target=\"{$menuItem['target']}\"") . " href=\"{$pluginMetadata['PLUGIN_URL']}/click.php?item={$menuItem['id']}&user_id={$userPrefs['id']}&location=@@LOCATION@@\">{$menuItem['title']}</a></li>";
-}
-
-$menuHtml = $cache->getCache($userPrefs['id']);
-if (!$menuHtml) {
-	/* build a set of regexp conditions to find this user's groups in serialized group lists hanging off of menu items */
-	$userGroups = 'FALSE';
-	// FIXME reimpliment groups
-	/*if (is_array($userPrefs['groups'])) {
-		$userGroups == '';
-		foreach($userPrefs['groups'] as $g) {
-			$userGroups = (strlen($userGroups) ? " {$userGroups} OR " : '') . "`groups` REGEXP 'a:[0-9]+\{(i:[0-9]+;i:[0-9]+;)*i:[0-9]+;i:{$g};(i:[0-9]+;i:[0-9]+;)*\}'";
-		}
-	}*/
-	
-	/* build the menus */
-	// TODO no doubt this could be elegantly wrapped up in a recursive function full of menu-related puns
-	$menuHtml = array();
-	$menus = $customPrefs->query("
+$cache->pushKey($_REQUEST['current_user']['id']);
+$cache->pushKey($_REQUEST['location']);
+$html = $cache->getCache('html');
+if (empty($html)) {
+	$response = $customPrefs->query("
 		SELECT *
-			FROM `menu-items`
+			FROM `users`
 			WHERE
-				`menu` IS NULL AND
+				`id` = '" . $_REQUEST['current_user']['id'] . "'
+	");
+	$user = $response->fetch_assoc();
+	
+	$query = "
+		SELECT m.`id`, m.`parent`, m.`order`, m.`title`, m.`subtitle`, m.`info`, m.`url`, m.`target`, m.`role`, acl.`group`, g.`user`
+			FROM `menu-items` AS m
+				LEFT JOIN `menu-acl` AS acl
+					ON acl.`menu-item` = m.`id`
+				LEFT JOIN `group-memberships` AS g
+					ON g.`group` = acl.`group`
+			WHERE
+				m.`parent` IS NULL OR (
+					m.`column` IS NOT NULL AND
+					m.`section` IS NOT NULL
+				) AND
 				(
-					`role` IS NULL OR
-					`role` = '" . $userPrefs['role'] . "'
-				) AND (
-					`groups` IS NULL OR
-					" . $userGroups . "
+					" . (empty($user['role']) ? '' : "`role` = '{$user['role']}' OR ") . "
+					`role` IS NULL
+				) AND
+				(
+					acl.`group` IS NULL OR
+					(
+						acl.`group` IS NOT NULL AND
+						g.`user` = '{$user['id']}'
+					)
 				)
 			ORDER BY
-				`order` ASC,
-				`title` ASC
-	");
-	for ($i = 0; $m = $menus->fetch_assoc(); $i++) {
-		$columns = $customPrefs->query("
-			SELECT *
-				FROM `menu-items`
-				WHERE
-					`menu` = '" . $m['id'] . "' AND
-					`column` IS NULL AND
-					(
-						`role` IS NULL OR
-						`role` = '" . $userPrefs['role'] . "'
-					) AND (
-						`groups` IS NULL OR
-						" . $userGroups . "
-					)
-				ORDER BY
-					`order` ASC,
-					`title` ASC
-		");
-		$html = startMenu('', $m, $columns->num_rows);
-		while ($c = $columns->fetch_assoc()) {
-			$html = startColumn($html, $c);
-			$sections = $customPrefs->query("
-				SELECT *
-					FROM `menu-items`
-					WHERE
-						`menu` = '" . $m['id'] . "' AND
-						`column` = '" . $c['id'] . "' AND
-						`section` IS NULL AND
-						(
-							`role` IS NULL OR
-							`role` = '" . $userPrefs['role'] . "'
-						) AND (
-							`groups` IS NULL OR
-							" . $userGroups . "
-						)
-					ORDER BY
-						`order` ASC,
-						`title` ASC
-			");
-			while ($s = $sections->fetch_assoc()) {
-				$html = startSection($html, $s);
-				$items = $customPrefs->query("
-					SELECT *
-						FROM `menu-items`
-						WHERE
-							`menu` = '" . $m['id'] . "' AND
-							`column` = '" . $c['id'] . "' AND
-							`section` = '" . $s['id'] . "' AND
-							(
-								`role` IS NULL OR
-								`role` = '" . $userPrefs['role'] . "'
-							) AND (
-								`groups` IS NULL OR
-								" . $userGroups . "
-							)
-						ORDER BY
-							`order` ASC,
-							`title` ASC
-				");
-				while ($item = $items->fetch_assoc()) {
-					$html = menuItem($html, $item);
-				}
-				$html = endSection($html);
-			}
-			$html = endColumn($html);
-		}
-		$menuHtml[$i] = endMenu($html);
-	}
-	$cache->setCache($userPrefs['id'], $menuHtml);
+				`parent` ASC,
+				`order` ASC
+	";
+	$response = $customPrefs->query($query);
 }
 
-$menuHtml = str_replace('@@LOCATION@@', $_SERVER['HTTP_REFERER'], $menuHtml);
-
-?>
-var global_navigation_menu = {
-	// types of user
-	// FIXME pull this from DB too
-	USER_CLASS_STUDENT: 'student',
-	USER_CLASS_STAFF: 'staff',
-	USER_CLASS_FACULTY: 'faculty',
-	USER_CLASS_NO_MENU: 'no-menu',
-	
-	// the class of the current user
-	userClass: <?= (strlen($userPrefs['role']) ? "'{$userPrefs['role']}'" : 'USER_CLASS_NO_MENU') ?>,
-			
-	appendMenus: function() {
-		// add the custom menu to the menubar
-		// if you wanted to add more menus, define another menu structure like resources and call appendMenu() with it as a parameter (menus would be added in the order that the appendMenu() calls occur)
-		if (this.userClass != this.USER_CLASS_NO_MENU) {
-			// append menus
-			$('#menu').after('<ul id="canvashack-global-navigation-menu"></ul>');
-<?php
-			foreach ($menuHtml as $html) {
-				echo "$('#canvashack-global-navigation-menu').append('$html');";
-			} ?>
-
-			/* initialize JQuery menu */			
-			$('#canvashack-global-navigation-menu').menu({position: {at: 'left bottom'}, icons: { submenu: "ui-icon-triangle-1-s" }});
-			
-			/* position add-on menu next to existing global navigation items */
-			$('#canvashack-global-navigation-menu').css('top', $('#menu').children().last().position().top);
-			$('#canvashack-global-navigation-menu').css('left', $('#menu').children().last().position().left + $('#menu').children().last().width());
-			
-			/* disable clicks on "noclick" menu items */
-			$('#canvashack-global-navigation-menu .noclick').addClass('ui-state-disabled').off('click');
+class MenuItem {
+	public function __construct($row) {
+		foreach ($row as $key => $value) {
+			$this->$key = $value;
 		}
 	}
-};
+	
+	public function __toString() {
+		return 	'<li class="ReactTray-list-item" >
+					<a href="' . $this->url . '" target="' . $this->target . '" class="ReactTray-list-item__link">' . $this->title . '</a>
+					<div class="ReactTray-list-item__helper-text" >' . $this->subtitle . '</div>
+				</li>';
 
-global_navigation_menu.appendMenus();
+	}
+}
+
+class Menu extends MenuItem {
+	public $items = [];
+	
+	public function __toString() {
+		return '<div id="canvashack_global-navigation-menu" style="position:absolute;background:#fff;" class="ReactTray__Content ReactTray__Content--after-open " tabindex="-1" >
+					<div class="ReactTray__layout" >
+						<div class="ReactTray__primary-content" >
+							<div class="ReactTray__header" >
+								<h1 class="ReactTray__headline" >' . $this->title . '</h1>
+								<button id="canvashack_tray_close" class="Button Button--icon-action ReactTray__closeBtn" type="button" >
+									<i class="icon-x" ></i>
+									<span class="screenreader-only" >Close</span>
+								</button>
+							</div>
+							<ul class="ReactTray__link-list" >' .
+								implode(PHP_EOL, $this->items) . PHP_EOL . 
+								'<!--<li class="ReactTray-list-item ReactTray-list-item--feature-item" >
+									<a href="@@Featured Item URL@@" >@@Featured Item@@</a>
+								</li>-->
+							</ul>
+						</div>
+						<div class="ReactTray__secondary-content" >
+							<div class="ReactTray__info-box" >
+								' . $this->info . '
+							</div>
+						</div>
+					</div>
+				</div>';
+
+	}
+}
+
+$menus = [];
+while ($item = $response->fetch_assoc()) {
+	if (empty($item['parent'])) {
+		$menus[$item['id']] = new Menu($item);
+	} else {
+		$menus[$item['parent']]->items[] = new MenuItem($item);
+	}
+}
+
+$trays = [];
+foreach ($menus as $id => $menu) {
+	$trays["tray$id"] = "tray$id: '" . preg_replace('/(\n|\r|\t)/', ' ', $menu) . "'";
+}
+
+?>
+var canvashack = {
+	trays: {
+		<?= implode (',' . PHP_EOL, $trays) ?>
+	},
+	
+	showMyTray: function(id) {
+		"use strict";
+		var self = this;
+		
+		/* show tray */
+		$('.ReactTrayPortal > div').addClass('ReactTray__Overlay ReactTray__Overlay--after-open').css('position', 'fixed').css('top', '0px').css('left', '0px').css('right', '0px').css('bottom', '0px').append(this.trays[id]);
+		
+		/* change menu item highlight */
+		var previousSelection = $('.ic-app-header__menu-list-item--active').removeClass('ic-app-header__menu-list-item--active');
+		$('#' + id).addClass('ic-app-header__menu-list-item--active');
+		$('#canvashack_tray_close').on('click', function () {
+			self.hideMyTray(id, previousSelection);
+		});
+		
+		/* allow other menu items to open over this tray */
+		$('#menu li:not(.canvashack.global-navigation-menu)').on('click', function () {
+			self.hideMyTray(id);
+		});
+		
+		$('#' + id).off('click.canvashack.global-navigation-menu').on('click.canvashack.global-navigation-menu', function () {
+			self.hideMyTray(id, previousSelection);
+		});
+	},
+	
+	hideMyTray: function(id, previousSelection) {
+		"use strict";
+		var self = this;
+		
+		/* hide my tray */
+		$('#canvashack_global-navigation-menu').remove();
+		$('#' + id).off('click.canvashack.global-navigation-menu').on('click.canvashack.global-navigation-menu', function() {
+			self.showMyTray(id);
+		});
+		
+		/* change menu item highlight back, etc. if just closing my tray */
+		if (previousSelection != undefined) {
+			$('.ic-app-header__menu-list-item--active').removeClass('ic-app-header__menu-list-item--active');
+			$('.ReactTray__Overlay').css('position', '').css('top', '').css('left', '').css('right', '').css('bottom', '');
+			$('.ReactTray__Overlay').removeClass('ReactTray__Overlay--after-open');
+			previousSelection.addClass('ic-app-header__menu-list-item--active');
+		}
+	},
+	
+	addMenuItem: function (title, url, id, before, after) {
+		"use strict";
+		var self = this;
+		
+		/* build menu item */
+		var menuItem =
+			'<li id="' + id + '" class="ic-app-header__menu-list-item canvashack_global-navigation-menu">' +
+				'<a href="' + url + '" class="ic-app-header__menu-list-link">' +
+					'<div class="menu-item-icon-container" aria-hidden="true">' +
+		            '</div>' +
+		            '<div class="menu-item__text">' + title + '</div>' +
+		        '</a>' +
+		    '</li>';
+		    
+		/* figure out where to put the menu item */
+		if(before != undefined) {
+			$('#menu li:has(a#' + before + ')').before(menuItem);
+		} else if (after !== undefined) {
+			$('#menu li:has(a#' + after + ')').after(menuItem);
+		} else {
+			$('#menu').append(menuItem);
+		}
+		    
+		    
+		$('#' + id).on('click.canvashack.global-navigation-menu', function() {
+			self.showMyTray(id);
+		});
+	},
+	
+	loadMenus: function() {
+		"use strict";
+		<?php
+			foreach ($menus as $id => $menu) {
+				echo "this.addMenuItem('{$menu->title}','{$menu->url}', 'tray{$id}', null, 'global_nav_profile_link');" . PHP_EOL;
+			}
+		?>
+	}
+};
