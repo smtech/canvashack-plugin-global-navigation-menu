7e8992c63356fb8c4d073f4094fc49b040ab27fe
Seth Battis <sethbattis@stmarksschool.org>
2016-08-22 16:04:40 -0400

    CustomPrefs

--- common.inc.php
+++ common.inc.php
@@ -1,16 +1,9 @@
 <?php
 
-require_once(__DIR__ . '/../common.inc.php');
+require_once __DIR__ . '/../common.inc.php';
 
 if (file_exists(__DIR__ . '/manifest.xml')) {
-	$manifest = simplexml_load_string(file_get_contents(__DIR__ . '/manifest.xml'));
+    $manifest = simplexml_load_string(file_get_contents(__DIR__ . '/manifest.xml'));
 }
 
 $pluginMetadata = new Battis\AppMetadata($sql, (string) $manifest->id);
-
-// FIXME le sigh… a more elegant password management scheme is needed here (save to config database, etc.)
-$customPrefs = mysqli_connect($secrets->mysql->customprefs->host, $secrets->mysql->customprefs->username, $secrets->mysql->customprefs->password, $secrets->mysql->customprefs->database);
-
-$cache = new Battis\HierarchicalSimpleCache($sql, basename(__DIR__));
-
-?>
